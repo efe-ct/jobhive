24e20c78b816441d0c89c75c1ad938216782bddf
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2025-08-15 21:00:06 +0100

    day1, initializing styles, navbar and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 
+import reactLogo from './assets/react.svg'
+import splash from './assets/unsplash-splash.jpg'
+import ladyImg from './assets/unsplash-officelady.jpg'
+
+import { navBarContent, heroImages } from './data/content'
+
 const App = () => {
   return (
     <>
      {/* Header */}
+      <header className="">
+        <div className="navbar">
+          <span>
+            <img src={reactLogo} alt="" />
+            <a href="">JobHive</a>
+          </span>
 
+          <input type="text" placeholder="Search for Jobs, Companies or keywords..." />
+
+          <nav className="navbar__links">
+            <a href="">{navBarContent[0].title}</a>
+            <a href="">{navBarContent[1].title}</a>
+            <a href="">{navBarContent[2].title}</a>
+            <a href="">{navBarContent[3].title}</a>
+            <a href="">{navBarContent[4].title}</a>
+          </nav>
+        </div>
+      </header>
+
+      <section id="hero">
+      {/* <section id="hero" style={{ backgroundColor: 'black'}}> */}
+        {/* <img style={{ width: '801px', height: '796px', flexShrink: 0 }} src={splash} alt="" /> */}
+        {/* <div className="image">
+          <img src={ladyImg} alt="" />
+        </div> */}
+
+        <div id="heroImage">
+          {heroImages.map((image) => (
+            <div style={{ zIndex: image.layer, inset: image.inset}} className="image" key={image.id} >
+              <img src={image.src} 
+              alt={image.description}
+              width={image.width}
+              height={image.height} />
+            </div>
+          ))}
+        </div>
+        
+        
+        {/* <div className="hero__caption">
+          <span>Find a Job that suits <br /> your interests and skills</span>
+        </div> */}
+        {/* <div style={{ width: '801px', height: '796px', flexShrink: 0 }} className="image" ></div> */}
+      </section>
     </>
   )
 }
---
+++ src/data/content.js
@@ -0,0 +1,71 @@
+export const navBarContent = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/"
+    },
+    {
+        id: 2,
+        title: "About",
+        path: "/about"
+    },
+    {
+        id: 3,
+        title: "Services",
+        path: "/services"
+    },
+    {
+        id: 4,
+        title: "Contact",
+        path: "/contact"
+    },
+    {
+        id: 5,
+        title: "Apply Now",
+        path: "/contact"
+    }
+]
+
+
+export const heroImages = [
+    // src: "/src/assets/unsplash-officelady.jpg",
+    // src: "/src/assets/constructionman1.png",
+    // src: "/src/assets/helpwanted.png",
+    // src: "/src/assets/vector.avif",
+    {
+        id: 0,
+        src: "/src/assets/hero/lady.png",
+        description: "An Office Lady with Glasses",
+        inset: "20em auto auto 7rem",
+        layer: 2,
+        width: "343.5px",
+        height: "515.5px",
+    },
+    {
+        id: 1,
+        src: "/src/assets/hero/construct.png",
+        description: "An Office Guy",
+        inset: "4rem auto auto 30rem",
+        layer: 3,
+        width: "343.5px",
+        height: "515.25px",
+    },
+    {
+        id: 2,
+        src: "/src/assets/hero/help.png",
+        description: "Help Wanted Banner",
+        inset: "30rem auto auto 27rem",
+        layer: 4,
+        width: "413px",
+        height: "515px",
+    },
+    {
+        id: 3,
+        src: "/src/assets/hero/vector.png",
+        description: "Illustraction",
+        inset: "50em auto auto auto",
+        layer: 1,
+        width: "413px",
+        height: "515px",
+    },
+]
